7b7589a71598d2a6cbccd47727aca50a5c539134
TanNhatCMS <mrkiet.edu@gmail.com>
2023-02-11 09:08:53 +0700

    Update OphimCrawlerServiceProvider.php

--- src/OphimCrawlerServiceProvider.php
+++ src/OphimCrawlerServiceProvider.php
@@ -58,6 +58,7 @@ class OphimCrawlerServiceProvider extends SP
 
     public function boot()
     {
+        $this->registerPolicies();
         $this->commands([
             CrawlerScheduleCommand::class,
         ]);
